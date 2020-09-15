bb50825d6b0221afcec969a2f98018185400c308
Daniel Hoffens <dhoffens@gmail.com>
2020-09-15 18:08:07 -0400

    setting up templates

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('Hello, world welcome to the polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
